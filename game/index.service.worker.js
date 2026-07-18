48722e7b886015d3e6611f7efef8bbf3345f30c9
includeleec <3683190+includeleec@users.noreply.github.com>
2026-07-18 07:29:50 -0700

    Add fullscreen button and aspect-preserving stretch mode

--- game/index.service.worker.js
+++ game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784383988|7187828';
+const CACHE_VERSION = '1784384894|7456110';
 /** @type {string} */
 const CACHE_PREFIX = 'Dodge the Creeps-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
