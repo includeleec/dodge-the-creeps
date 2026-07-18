c1eafc0b68d823d888f080f3bd592dd47d4dd2f0
includeleec <3683190+includeleec@users.noreply.github.com>
2026-07-18 07:13:48 -0700

    Add mobile touch controls: drag to move, hold to auto-fire, on-screen pause button

--- game/index.service.worker.js
+++ game/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784376302|8134923';
+const CACHE_VERSION = '1784383988|7187828';
 /** @type {string} */
 const CACHE_PREFIX = 'Dodge the Creeps-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
